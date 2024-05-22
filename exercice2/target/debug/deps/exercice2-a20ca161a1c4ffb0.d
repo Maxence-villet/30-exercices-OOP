ed6e48bb7a5cc8e69c3cf8287a4468ddb157f96e
Maxence.villet <maxence.villetschoumaker@gmail.com>
2024-05-22 12:52:54 +0200

    exercice 1 finish

---
+++ exercice2/target/debug/deps/exercice2-a20ca161a1c4ffb0.d
@@ -0,0 +1,5 @@
+C:\Users\User\Desktop\30-exercices-OOP\exercice2\target\debug\deps\libexercice2-a20ca161a1c4ffb0.rmeta: src\main.rs
+
+C:\Users\User\Desktop\30-exercices-OOP\exercice2\target\debug\deps\exercice2-a20ca161a1c4ffb0.d: src\main.rs
+
+src\main.rs:
